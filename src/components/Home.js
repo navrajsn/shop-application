f9e36b459efb4fea3c8ba83caa512b3b8f5a2c4a
Navraj Saini <navraj.saini55@gmail.com>
2022-07-14 17:08:35 +1000

    Adding product card, random image and styles.Image to be updated later

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { ShopContext } from './context/Context'
-
+import ProductCard from './ProductCard'
+import './styles.css'
 const Home = () => {
   const { products } = ShopContext()
-  console.log(products)
   return (
     <>
       {products ? (
         <div className="home">
-          {products.map((product) => (
-            <span>{product.productName}</span>
-          ))}
+          <div className="productContainer">
+            {products.map((product) => {
+              return <ProductCard key={product.index} product={product} />
+            })}
+          </div>
         </div>
       ) : (
         <div>loading</div>
---
+++ src/components/ProductCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="products">
+      <Card>
+        <Card.Img variant="top" src="Product_1.jpeg" />
+        <Card.Body>
+          <Card.Title style={{ width: 200 }}>{product.productName}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>{product.price}</span>
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default ProductCard
