9c5b7b18a906d0be08a14f86bd9f2da3efe93ef2
Navraj Saini <navraj.saini55@gmail.com>
2022-07-14 16:26:59 +1000

    Added axxios to package for fetching data from data.json, updated context with value, update home page to see all the productNames.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { ShopContext } from './context/Context'
 
 const Home = () => {
-  return <div>Home page</div>
+  const { products } = ShopContext()
+  console.log(products)
+  return (
+    <>
+      {products ? (
+        <div className="home">
+          {products.map((product) => (
+            <span>{product.productName}</span>
+          ))}
+        </div>
+      ) : (
+        <div>loading</div>
+      )}
+    </>
+  )
 }
 
 export default Home
--- src/components/context/Context.js
+++ src/components/context/Context.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { createContext } from 'react'
+import { createContext, useEffect, useState, useContext } from 'react'
+import axios from 'axios'
 
 const Shop = createContext()
 
 const Context = ({ children }) => {
-  return <Shop.Provider>{children}</Shop.Provider>
+  const [products, setProducts] = useState()
+  useEffect(() => {
+    axios
+      .get('data.json')
+      .then((res) => {
+        console.log(res.data)
+        setProducts(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return <Shop.Provider value={{ products }}>{children}</Shop.Provider>
 }
 
 export default Context
+
+export const ShopContext = () => {
+  return useContext(Shop)
+}
